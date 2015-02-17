a16bd6a938bed36047e74f6dd88835307b0cefe1
Jeff Black <jeffblack360@gmail.com>
2015-02-17 17:24:49 -0600

    Thread lock example

---
+++ threads-lock.pl
@@ -0,0 +1,39 @@
+#!/mtg/tool/bin/perl
+use warnings;
+use strict;
+use autodie;
+use feature qw(say);
+
+use threads;
+use threads::shared;
+
+my $total :shared = 0;
+
+sub sub1
+{
+    say "Starting thread: ". shift;
+
+    while (1)
+    {
+        my $result = 0;
+
+      {
+        lock($total);
+        $total += $result;
+      }
+
+      last if $result == 0;
+    }
+
+    return;
+}
+
+my $thr1 = threads->create(\&sub1, "thr1");
+my $thr2 = threads->create(\&sub1, "thr2");
+my $thr3 = threads->create(\&sub1, "thr3");
+
+$thr1->join();
+$thr2->join();
+$thr3->join();
+
+say "total=$total";
